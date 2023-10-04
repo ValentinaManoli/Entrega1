6abe9af8d737062b85cd46652714f589f725e59e
Valentina Manoli <valumanoli@gmail.com>
2023-10-03 21:58:45 -0300

    crud2

--- Entrega final/models/cursoModel.js
+++ Entrega final/models/cursoModel.js
@@ -9,5 +9,42 @@ async function getCurso(){
 
 }
 
+async function deleteCursoById(id){
+        var query = 'delete from cursos where id = ?'
+        var rows = await pool.query(query, [id]);
+        return rows;
+}
 
-module.exports = {getCurso}
+
+async function insertCurso(obj){
+        try{
+            var query= 'insert into cursos set ?'
+            var rows = await pool.query(query, [obj]);
+            return rows;
+        } catch (error){
+            console.log(error);
+            throw error;
+        }
+    }
+
+async function getCursoById(id){
+    var query = 'select * from cursos where id = ?';
+    var rows = await pool.query(query, [id]);
+
+    return rows [0];
+}
+
+async function modificarCursoById(obj, id){
+    try{
+        var query = 'update cursos set ? where id=?';
+    var rows = await pool.query(query, [obj, id]);
+
+    return rows;
+    } catch (error){
+        throw error;
+    }
+    
+}
+    
+    module.exports = {getCurso, deleteCursoById, insertCurso, getCursoById, modificarCursoById}
+
--- Entrega final/routes/admin/cursos.js
+++ Entrega final/routes/admin/cursos.js
@@ -16,5 +16,74 @@ router.get('/', async function(req, res, next) {
   });
 });
 
+router.get('/eliminar/:id', async function(req, res, next) {
+
+  var id = req.params.id;
+
+  await cursoModel.delteCursosById(id);
+  res.redirect('/admin/cursos');
+});
+
+
+
+router.get('agregar', (req, res, next)=> {
+  res.render('admin/cursos/agregar', {
+    layout: 'admin/layout'
+  })
+})
+
+router.post('/agregar', async (req, res, next) => {
+  try{
+    if (req.body.curso != "" && req.body.unidad !="" && req.body.tarea !=""){
+      await cursosModel.insertCurso(req.body);
+      res.redirect('/admin/cursos')
+    } else {
+      res.render ('admin/agregar', {
+        layout: 'admin/layout',
+        error : true,
+        message: 'Todos los campos son requeridos'
+      })
+    }
+  } catch(error) {
+    console.log (error)
+    res.render('admin/agregar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se cargó la nueva tarea'
+    })
+  }
+})
+
+router.get('/modificar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  console.log(req.params.id);
+  var curso = await cursosModel.getCursoById(id);
+
+  res.render('admin/modificar', {
+    layout: 'admin/layout', 
+    curso
+  })
+})
+
+
+router.post('/modificar', async (req, res, next) => {
+  try{
+    var obj = {
+      curso: req.body.curso,
+      unidad: req.body.unidad,
+      tarea: req.body.tarea
+    }
+    console.log(obj)
+    await cursosModel.modificarCursoById(obj, req.body.id);
+    res.redirect('/admin/cursos')
+  } catch(error) {
+    console.log (error)
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se modificó la tarea'
+    })
+  }
+})
 
 module.exports= router;
